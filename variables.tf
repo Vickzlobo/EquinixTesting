e7096d606ccc221ecbe188d0bfcc75c31fa78b4f
Vickzlobo <vickzlobo@gmail.com>
2024-09-18 07:56:42 +0800

    This is the variables file for FCR

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "FCRname" {}
+variable "FCRlocation" {}
+variable "FCRtype" {}
+variable "emails" {}
+variable "Equinixpurchaseorder" {}
+variable "FCRmetrocode" {}
+variable "FCRpackage" {}
+variable "Equinixprojectid" {}
+variable "account_number" {}
